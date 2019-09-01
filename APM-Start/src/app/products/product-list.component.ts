b1444e50a5f2d8c22a270102d7a8a9246ec6c8a2
Luis Velandia <luiseduardo1218@hotmail.com>
2019-08-31 19:46:40 -0500

    master-10: call web service

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -14,6 +14,7 @@ export class ProductListComponent implements OnInit {
     showImage: boolean = false;
     filteredProducts: IProduct[];
     products: IProduct[];
+    errorMessage: string;
     _listFilter: string;
     get listFilter(): string {
         return this._listFilter;
@@ -27,8 +28,13 @@ export class ProductListComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        this.products = this.productService.getProducts();
-        this.filteredProducts = this.products;
+        this.productService.getProducts().subscribe({
+            next: products => {
+                this.products = products;
+                this.filteredProducts = this.products;
+            },
+            error: err => this.errorMessage = err
+        });
     }
 
     toggleImage(): void {
