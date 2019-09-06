77161cdd1510e6b816abc5fde6adc67172a0bc8a
Luis Velandia <luiseduardo1218@hotmail.com>
2019-09-05 22:06:03 -0500

    master-13: shared module

--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -1,30 +1,26 @@
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
-import { StarComponent } from './../shared/star.component';
 import { ConvertToSpacePipe } from './../shared/convert-to-space.pipe';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailGuard } from './product-detail.guard';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
     RouterModule.forChild([
       { path: 'products', component: ProductListComponent },
       { path: 'products/:id',
         canActivate: [ProductDetailGuard ],
         component: ProductDetailComponent }
-    ])
+    ]),
+    SharedModule
   ],
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    ConvertToSpacePipe,
-    StarComponent
+    ConvertToSpacePipe
   ]
 })
 export class ProductModule { }
---
+++ APM-Start/src/app/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { FormsModule } from '@angular/forms';
+import { StarComponent } from './star.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    StarComponent
+  ],
+  exports: [
+    StarComponent,
+    CommonModule,
+    FormsModule
+  ]
+})
+export class SharedModule { }
